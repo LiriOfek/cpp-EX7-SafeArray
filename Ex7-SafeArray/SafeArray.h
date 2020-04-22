992476a150a7c8fdeb29474b6739399b1a251fc8
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-22 12:44:45 +0300

    add header file of SafeArray with documentation

---
+++ Ex7-SafeArray/SafeArray.h
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: SafeArray.h
+Author: Liri (22/4/2020)
+Purpose: This header file contain class of SafeArray, with parameters
+		 of the array and the array size, and declaration of functions that push,
+		 pop elements from the array, get element and erase array
+\********************************************************/
