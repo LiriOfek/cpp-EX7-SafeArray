d2ff40ae6a261afeae0a9899a90852cfd2adcde9
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-22 12:48:13 +0300

    add main file with documentation

---
+++ Ex7-SafeArray/main.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (22/4/2020)
+Purpose: This file contain tests for functions in SafeArray.cpp,
+		 make array of chars and ints, add elements to them, remove
+		 elements and print the array
+\********************************************************/
